f3125ee66ce53dcd6e4690a4288e803e49ec8345
dufou <dufou@Gautier>
2017-05-07 02:00:51 +0200

    refactoring dimension et position du vaisseau

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -1,29 +1,63 @@
 package fr.unilim.iut.spaceinvaders;
 
 public class Vaisseau {
-	int x;
-	int y;
+	Position origine;
+	Dimension dimension;
 
-	public Vaisseau(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Vaisseau(int longueur, int hauteur) {
+		this(longueur, hauteur, 0, 0);
+	}
+
+	public Vaisseau(int longueur, int hauteur, int x, int y) {
+		this(new Dimension(longueur, hauteur), new Position(x, y));
+	}
+
+	public Vaisseau(Dimension dimension, Position positionOrigine) {
+		this.dimension = dimension;
+		this.origine = positionOrigine;
 	}
 
 	public boolean occupeLaPosition(int x, int y) {
-		return (this.x==x) && (this.y==y);
+		return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
+	}
+
+	private boolean estOrdonneeCouverte(int y) {
+		return (ordonneeLaPlusHaute() <= y) && (y <= ordonneeLaPlusBasse());
+	}
+
+	private boolean estAbscisseCouverte(int x) {
+		return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
+	}
+
+	private int ordonneeLaPlusBasse() {
+		return this.origine.ordonnee();
+	}
+
+	private int ordonneeLaPlusHaute() {
+		return ordonneeLaPlusBasse() - this.dimension.hauteur() + 1;
+	}
+
+	public int abscisseLaPlusADroite() {
+		return abscisseLaPlusAGauche() + this.dimension.longueur() - 1;
 	}
 
 	public void seDeplacerVersLaDroite() {
-		 this.x = this.x + 1 ;
-		
+		this.origine.changerAbscisse(this.abscisseLaPlusAGauche() + 1);
+
 	}
 
-	public int abcisse() {
-		return this.x;
+	public int abscisseLaPlusAGauche() {
+		return this.origine.abscisse();
 	}
 
 	public void seDeplacerVersLaGauche() {
-		 this.x = this.x - 1 ;
-		
+		this.origine.changerAbscisse(this.abscisseLaPlusAGauche() - 1);
+
+	}
+
+	public void positionner(int x, int y) {
+		this.origine.changerAbscisse(x);
+		this.origine.changerOrdonnee(y);
+
 	}
 }
